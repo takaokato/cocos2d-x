4ca70ec7595fd05a96648cbc95154855c6f2d1e0
Takao Kato <takao@nyahoon.com>
2016-04-01 02:46:53 +0800

    remove global constructor calls

--- cocos/platform/android/javaactivity-android.cpp
+++ cocos/platform/android/javaactivity-android.cpp
@@ -39,11 +39,6 @@ THE SOFTWARE.
 #include "network/CCDownloader-android.h"
 #include <android/log.h>
 #include <jni.h>
-#include <signal.h>
-#include <dlfcn.h>
-#include <unwind.h>
-#include <ucontext.h>
-#include <cxxabi.h>
 
 #define  LOG_TAG    "main"
 #define  LOGD(...)  __android_log_print(ANDROID_LOG_DEBUG,LOG_TAG,__VA_ARGS__)
@@ -52,77 +47,13 @@ void cocos_android_app_init(JNIEnv* env) __attribute__((weak));
 
 using namespace cocos2d;
 
-static void ExitWithStackTrace(int sig_num, siginfo_t * info, void * ucontext)
-{
-	ucontext_t *uc = (ucontext_t*)ucontext;
-#if defined(__arm__)
-	// reference:
-	// http://stackoverflow.com/questions/5397041/getting-the-saved-instruction-pointer-address-from-a-signal-handler
-	// http://stackoverflow.com/questions/15752188/arm-link-register-and-frame-pointer
-	void* pc = (void**)uc->uc_mcontext.arm_pc;
-	void* sp = (void**)uc->uc_mcontext.arm_sp;
-	void** fp = (void**)uc->uc_mcontext.arm_fp;
-	void* lr = (void*)uc->uc_mcontext.arm_lr;
-	Dl_info dlInfo;
-	if (dladdr(pc, &dlInfo)) {
-		int status;
-		const char* symbol = dlInfo.dli_sname;
-		char* demangledName = abi::__cxa_demangle(symbol, NULL, NULL, &status);
-		if (status == 0) {
-			symbol = demangledName;
-		}
-		__android_log_print(ANDROID_LOG_ERROR, "SIGNAL", "%s(%p) at %s", strsignal(sig_num), info->si_addr, symbol);
-		if (demangledName != NULL) {
-			free(demangledName);
-		}
-	}
-	else {
-		__android_log_print(ANDROID_LOG_ERROR, "SIGNAL", "%s(%p) at PC(%p)", strsignal(sig_num), info->si_addr, pc);
-	}
-	__android_log_print(ANDROID_LOG_ERROR, "SIGNAL", "sp:%p, fp:%p, lr:%p, pc:%p", sp, fp, lr, pc);
-
-	__android_log_print(ANDROID_LOG_ERROR, "Stack Trace", "Start");
-	for ( ; ; ) {
-		if (dladdr(lr, &dlInfo)) {
-			int status;
-			const char* symbol = dlInfo.dli_sname;
-			char* demangledName = abi::__cxa_demangle(symbol, NULL, NULL, &status);
-			if (status == 0) {
-				symbol = demangledName;
-			}
-			__android_log_print(ANDROID_LOG_ERROR, "Stack Trace", "%p:<%s>+%p", lr, symbol, dlInfo.dli_saddr);
-			if (demangledName != NULL) {
-				free(demangledName);
-			}
-		}
-		else {
-			__android_log_print(ANDROID_LOG_ERROR, "Stack Trace", "%p", lr);
-		}
-		if (fp == NULL || (void*)fp < sp) {
-			sp = fp;
-			lr = fp[-1];
-			fp = (void**)fp[-3];
-		}
-		else {
-			break;
-		}
-	}
-	__android_log_print(ANDROID_LOG_ERROR, "Stack Trace", "End");
-#endif
-	exit(-1);
-}
 
 extern "C"
 {
 
 JNIEXPORT jint JNI_OnLoad(JavaVM *vm, void *reserved)
 {
-// now we are using native debugger, don't need to dump stack trace.
-//	struct sigaction sigact;
-//	sigact.sa_sigaction = ExitWithStackTrace;
-//	sigact.sa_flags = SA_SIGINFO;
-//	sigaction(SIGABRT, &sigact, (struct sigaction *)NULL);
-//	sigaction(SIGSEGV, &sigact, (struct sigaction *)NULL);
+    LOGD("JNI_OnLoad");
 
 	JniHelper::setJavaVM(vm);
 
