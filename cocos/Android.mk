20c7b0197d88c4d688f3b5ce37ecba2d6916bb0c
Takao Kato <takao@nyahoon.com>
2016-06-13 09:28:00 +0800

    removed unused modules from Android.mk

--- cocos/Android.mk
+++ cocos/Android.mk
@@ -269,8 +269,8 @@ LOCAL_STATIC_LIBRARIES += cocos_tiff_static
 LOCAL_STATIC_LIBRARIES += cocos_webp_static
 LOCAL_STATIC_LIBRARIES += cocos_chipmunk_static
 LOCAL_STATIC_LIBRARIES += cocos_zlib_static
-LOCAL_STATIC_LIBRARIES += recast_static
-LOCAL_STATIC_LIBRARIES += bullet_static
+#LOCAL_STATIC_LIBRARIES += recast_static
+#LOCAL_STATIC_LIBRARIES += bullet_static
 
 LOCAL_WHOLE_STATIC_LIBRARIES := cocos2dxandroid_static
 
@@ -290,10 +290,12 @@ include $(CLEAR_VARS)
 LOCAL_MODULE := cocos2dx_static
 LOCAL_MODULE_FILENAME := libcocos2d
 
-LOCAL_STATIC_LIBRARIES := cocostudio_static
-LOCAL_STATIC_LIBRARIES += cocosbuilder_static
-LOCAL_STATIC_LIBRARIES += cocos3d_static
-LOCAL_STATIC_LIBRARIES += spine_static
+#LOCAL_STATIC_LIBRARIES := cocostudio_static
+LOCAL_STATIC_LIBRARIES := cocos_ui_static
+LOCAL_STATIC_LIBRARIES += cocosdenshion_static
+#LOCAL_STATIC_LIBRARIES += cocosbuilder_static
+#LOCAL_STATIC_LIBRARIES += cocos3d_static
+#LOCAL_STATIC_LIBRARIES += spine_static
 LOCAL_STATIC_LIBRARIES += cocos_network_static
 LOCAL_STATIC_LIBRARIES += audioengine_static
 
@@ -307,17 +309,17 @@ $(call import-module,jpeg/prebuilt/android)
 $(call import-module,tiff/prebuilt/android)
 $(call import-module,webp/prebuilt/android)
 $(call import-module,chipmunk/prebuilt/android)
-$(call import-module,3d)
+#$(call import-module,3d)
 $(call import-module,audio/android)
-$(call import-module,editor-support/cocosbuilder)
-$(call import-module,editor-support/cocostudio)
-$(call import-module,editor-support/spine)
+#$(call import-module,editor-support/cocosbuilder)
+#$(call import-module,editor-support/cocostudio)
+#$(call import-module,editor-support/spine)
 $(call import-module,network)
 $(call import-module,ui)
 $(call import-module,extensions)
-$(call import-module,Box2D)
-$(call import-module,bullet)
-$(call import-module,recast)
+#$(call import-module,Box2D)
+#$(call import-module,bullet)
+#$(call import-module,recast)
 # $(call import-module,curl/prebuilt/android)
 $(call import-module,websockets/prebuilt/android)
-$(call import-module,flatbuffers)
+#$(call import-module,flatbuffers)
